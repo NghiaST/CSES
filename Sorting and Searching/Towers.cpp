b8bb2b56d0fea7490e38e7a3da42f78d87a89434
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-07-29 22:06:17 +0700

    Create Towers.cpp

---
+++ Sorting and Searching/Towers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, r, x, Q[200005];
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n;
+    for(int i=1; i<=n; ++i) {
+        cin >> x;
+        if (!r || Q[r] <= x) Q[++r] = x;
+        else *upper_bound(Q+1, Q+r, x) = x;
+    }
+    cout << r;
+}
