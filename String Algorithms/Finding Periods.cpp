68d8da3a14cca2aac74270af8cab051009330209
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-08-10 19:35:55 +0700

    Create Finding Periods.cpp

---
+++ String Algorithms/Finding Periods.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+string s;
+int n, z[1000005];
+ 
+int main()
+{
+    getline(cin, s); n = s.length();
+    for(int i=1, l=0, r=-1; i<=n; ++i) {
+        if (i <= r) z[i] = min(r-i+1, z[i-l]);
+        while (i + z[i] < n && s[i + z[i]] == s[z[i]]) ++z[i];
+        if (i + z[i] > r) l = i, r = i + z[i] - 1;
+        if (i + z[i] == n) cout << i << ' ';
+    }
+}
