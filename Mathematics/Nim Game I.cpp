2b4f0c104820401d28bc8dbf3d66cb1c68783db3
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-09-06 11:07:23 +0700

    Create Nim Game I.cpp

---
+++ Mathematics/Nim Game I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int t, n;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int x = 0, y;
+        while (n--) cin >> y, x ^= y;
+        cout << (x ? "first\n" : "second\n");
+    }
+}
