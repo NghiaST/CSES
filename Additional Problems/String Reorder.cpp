1ed1dc9fb31147d626ac3351591ce0607c3e449a
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-09-06 11:54:33 +0700

    Create String Reorder.cpp

---
+++ Additional Problems/String Reorder.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+string s;
+int c[256], maxi, prv;
+ 
+int main()
+{
+    getline(cin, s);
+    for(char i : s) ++c[i];
+    for(int i=s.length(); i; --i) {
+        int maxi = 0;
+        for(int j='A'; j<='Z'; ++j)
+            if (j != prv && c[maxi] < c[j]) maxi = j;
+        if (c[maxi] * 2 - 1 > i) {
+            cout << -1;
+            return 0;
+        }
+        if (c[maxi] * 2 > i) prv = maxi;
+        else {
+            for(int j='A'; j<='Z'; ++j)
+            if (j != prv && c[j]) {
+                prv = j;
+                break;
+            }
+        }
+        --c[prv];
+        putchar(prv);
+    }
+}
