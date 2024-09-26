7239e17134d958d257cff3a69ba4e755fd128e80
nghiast <nghiass001@gmail.com>
2024-09-26 15:02:52 +0700

    Resolve 2 hacking problems

--- Geometry/Point in Polygon.cpp
+++ Geometry/Point in Polygon.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 using namespace std;
 int n, q;
-struct point{long long x, y;} a[1005];
-const point INF_point = {0x3c3c3c3c, 0};
+struct point {
+    long long x, y;
+    point operator - (point a) { return {x - a.x, y - a.y}; }
+} a[1005];
  
-long long acreage(point A, point B, point C) {
-    long long w = A.x * (C.y - B.y) + B.x * (A.y - C.y) + C.x * (B.y - A.y);
+long long cross(point a, point b) { return a.x * b.y - a.y * b.x; }
+long long area(point a, point b, point c) { 
+    long long w = cross(b - a, c - a);
     if (w < 0) return -1;
     else if (w > 0) return 1;
     else return 0;
 }
  
-int Intersect(point a, point b, point c, point d) {
-    long long area1 = acreage(a, b, c);
-    long long area2 = acreage(a, b, d);
-    long long area3 = acreage(c, d, a);
-    long long area4 = acreage(c, d, b);
-    if (!area1 && !area2) {
-        if (min(a.x, b.x) > max(c.x, d.x) || min(c.x, d.x) > max(a.x, b.x) ||
-            min(a.y, b.y) > max(c.y, d.y) || min(c.y, d.y) > max(a.y, b.y)) return 0;
-        else return 1;
-    }
-    else if (area1 * area2 < 0 && area3 * area4 < 0) return 2;
-    else return 0;
-}
- 
 int where_with_polygon(point w) {
-    for(int i=1; i<=n; ++i) if (!acreage(w, a[i-1], a[i])) {
-        if (min(a[i-1].x, a[i].x) <= w.x && w.x <= max(a[i-1].x, a[i].x) &&
-            min(a[i-1].y, a[i].y) <= w.y && w.y <= max(a[i-1].y, a[i].y))
-            return 1;
+    bool inside = false, touch = false;
+    for(int i = 1; i <= n; ++i) {
+        point p1 = a[i - 1], p2 = a[i];
+        long long area1 = area(p1, p2, w);
+        if (!area1 &&
+            min(p1.x, p2.x) <= w.x && w.x <= max(p1.x, p2.x) &&
+            min(p1.y, p2.y) <= w.y && w.y <= max(p1.y, p2.y)) 
+            touch = true;
+ 
+        if ((p1.y > w.y) != (p2.y > w.y) &&
+            (((p2.y - p1.y) ^ area1) >> 63) == 0)
+            inside = !inside;
     }
-    int ret = 0;
-    for(int i=1; i<=n; ++i) ret += Intersect(w, INF_point, a[i-1], a[i]);
-    return (ret & 2);
+    if (touch) return -1;
+    return inside;
 }
  
 int main()
@@ -41,13 +37,13 @@ int main()
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     cin >> n >> q;
     for(int i=1; i<=n; ++i) cin >> a[i].x >> a[i].y;
-    point w;
     a[0] = a[n];
     while (q--) {
+        point w;
         cin >> w.x >> w.y;
         int ans = where_with_polygon(w);
-        if (ans == 0) cout << "OUTSIDE\n";
-        else if (ans == 1) cout << "BOUNDARY\n";
+        if (ans == -1) cout << "BOUNDARY\n";
+        else if (ans == 0) cout << "OUTSIDE\n";
         else cout << "INSIDE\n";
     }
 }
--- Mathematics/Candy Lottery.cpp
+++ Mathematics/Candy Lottery.cpp
@@ -2,13 +2,13 @@
 #include <iomanip>
 using namespace std;
 int n, k;
-long double res;
+double res;
  
 int main()
 {
     cin >> n >> k;
     for(int i=1; i<=k; ++i) {
-        long double x = 1, y = 1;
+        double x = 1, y = 1;
         for(int j=1; j<=n; ++j) {
             x = x * i / k;
             y = y * (i - 1) / k;
